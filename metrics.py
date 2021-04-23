dd54339682d3a2b43b81c2968410c12bc58409c3
Chih-Jen Lin <cjlin@csie.ntu.edu.tw>
2021-04-23 17:21:56 +0800

    rename some variables in metrics.py

--- metrics.py
+++ metrics.py
@@ -1,25 +1,20 @@
-"""Metrics different to sklearn are placed here.
-Some of the functions are from CAML-MIMIC:
-(https://github.com/jamesmullenbach/caml-mimic/blob/master/evaluation.py)."""
-
+"""Metrics modified from CAML-MIMIC are tentatively placed here.
+(https://github.com/jamesmullenbach/caml-mimic/blob/master/evaluation.py)
+They are used for the internal need to compare with CAML."""
 
 import numpy as np
 
-
-def intersect_size(y_true, y_pred, axis):
-    # axis=0 for label-level union (macro). axis=1 for instance-level
+def intersection(y_true, y_pred, axis):
+    # y_true and y_pred are #data * #labels matrices
     return np.logical_and(y_pred, y_true).sum(axis=axis).astype(float)
 
-
 def macro_precision(y_true, y_pred):
-    num = intersect_size(y_true, y_pred, 0) / (y_pred.sum(axis=0) + 1e-10)
-    return np.mean(num)
-
+    precision_per_label = intersection(y_true, y_pred, 0) / (y_pred.sum(axis=0) + 1e-10)
+    return np.mean(precision_per_label)
 
 def macro_recall(y_true, y_pred):
-    num = intersect_size(y_true, y_pred, 0) / (y_true.sum(axis=0) + 1e-10)
-    return np.mean(num)
-
+    recall_per_label = intersection(y_true, y_pred, 0) / (y_true.sum(axis=0) + 1e-10)
+    return np.mean(recall_per_label)
 
 def macro_f1(y_true, y_pred):
     prec = macro_precision(y_true, y_pred)
@@ -27,10 +22,9 @@ def macro_f1(y_true, y_pred):
     f1 = 2 * (prec * rec) / (prec + rec + 1e-10)
     return f1
 
-
-def precision_at_k(y_true, y_pred, k):
+def precision_at_k(y_true, y_pred_vals, k):
     # num true labels in top k predictions / k
-    sortd = np.argsort(y_pred)[:,::-1]
+    sortd = np.argsort(y_pred_vals)[:,::-1]
     topk = sortd[:,:k]
 
     # get precision at k for each example
@@ -43,10 +37,9 @@ def precision_at_k(y_true, y_pred, k):
 
     return np.mean(vals)
 
-
-def recall_at_k(y_true, y_pred, k):
+def recall_at_k(y_true, y_pred_vals, k):
     # num true labels in top k predictions / num true labels
-    sortd = np.argsort(y_pred)[:,::-1]
+    sortd = np.argsort(y_pred_vals)[:,::-1]
     topk = sortd[:,:k]
 
     # get recall at k for each example
