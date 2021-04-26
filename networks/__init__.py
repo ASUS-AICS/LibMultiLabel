d77cbac5bf15145ffdcf9dc1d2def06264a8c466
Si-An <sian1_chen@asus.com>
2021-04-26 17:02:42 +0000

    set weight init method

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,2 +1,11 @@
+import torch.nn as nn
+
 from networks.caml import CAML
 from networks.kim_cnn import KimCNN
+
+
+def get_weight_init_func(config):
+    def init_weights(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+            getattr(nn.init, config.weight_init + '_')(m.weight)
+    return init_weights
