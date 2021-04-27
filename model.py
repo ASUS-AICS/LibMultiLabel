31647fd85484795a1b4d63b96bd3a7d5456174dd
Eleven Liu <eleven1_liu@asus.com>
2021-04-27 09:50:58 +0800

    Update log info.
    Co-authored-by: Chih-Jen Lin <cjlin@csie.ntu.edu.tw>

--- model.py
+++ model.py
@@ -96,7 +96,7 @@ class Model(object):
 
                 self.train_epoch(train_loader)
 
-                logging.info('Start validate Dev Dataset')
+                logging.info('Start predicting a validation set')
                 val_metrics = evaluate(self.config, self, val_loader)
 
                 if val_metrics[self.config.val_metric] >= self.best_metric:
