2d1cc8583e281d002a4417da2c554751093590a4
Eleven Liu <Eleven1_liu@asus.com>
2021-05-05 07:34:26 +0000

    remove unused import

--- libmultilabel/networks/__init__.py
+++ libmultilabel/networks/__init__.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 from libmultilabel.networks.caml import CAML
 from libmultilabel.networks.kim_cnn import KimCNN
 from libmultilabel.networks.xml_cnn import XMLCNN
-from libmultilabel.networks.cnn import VanillaConv
 
 
 def get_init_weight_func(config):
