2467866a36727d1cd87fc27eb4ff0f859b75bd2b
Eleven Liu <eleven1_liu@asus.com>
2021-05-08 16:21:55 +0000

    add label_key and num_classes to evaluate

--- libmultilabel/evaluate.py
+++ libmultilabel/evaluate.py
@@ -9,13 +9,24 @@ from .metrics import another_macro_f1, precision_recall_at_ks
 from .utils import Timer, dump_log, save_top_k_prediction
 
 
-def evaluate(model, dataset_loader, monitor_metrics, predict_out_path=None, save_k_predictions=0):
+def evaluate(model, dataset_loader, num_classes, monitor_metrics, label_key='label', predict_out_path=None, save_k_predictions=0):
+    """Evaluate model and save top-k prediction to file if the number of top_k_prediction > 0.
+
+    Args:
+        model (Model): a high level class used to initialize network, predict examples and load/save model
+        dataset_loader (DataLoader)
+        num_classes (int): the number of the labels
+        monitor_metrics (list): metrics to monitor while validating
+        label_key (str, optional): the key to label in the dataset. Defaults to 'label'.
+        predict_out_path (str, optional): path to file for saving the top k predictions. Defaults to None.
+        save_k_predictions (int, optional): top k label scores to save for each sample. Defaults to 0.
+    """
     timer = Timer()
     progress_bar = tqdm(dataset_loader)
-    eval_metric = MultiLabelMetrics(num_classes=len(model.classes), monitor_metrics=monitor_metrics)
+    eval_metric = MultiLabelMetrics(num_classes, monitor_metrics=monitor_metrics)
 
     for batch in progress_bar:
-        batch_labels = batch['label']
+        batch_labels = batch[label_key] # set label_key in os.env or sync naming
         predict_results = model.predict(batch)
         batch_label_scores = predict_results['scores']
 
@@ -44,9 +55,9 @@ class MultiLabelMetrics():
     def add_values(self, y_true, y_pred):
         """Add batch of y_true and y_pred.
 
-        Parameters:
-        y_true (ndarray): a 2D array with ground truth labels (shape: batch_size * number of classes)
-        y_pred (ndarray): a 2d array with predicted labels (shape: batch_size * number of classes)
+        Args:
+            y_true (ndarray): a 2D array with ground truth labels (shape: batch_size * number of classes)
+            y_pred (ndarray): a 2d array with predicted labels (shape: batch_size * number of classes)
         """
         self.y_true.append(y_true)
         self.y_pred.append(y_pred)
