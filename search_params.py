6fb7ca6b2abb18d57daa64db85775cb9a6b5765d
Eleven Liu <Eleven1_Liu@asus.com>
2021-06-03 07:05:32 +0000

    (1) Use basic_variant to reduce the confusion between search space and search algorithm. (2) Add grid search example.

--- search_params.py
+++ search_params.py
@@ -100,7 +100,8 @@ def main():
     parser.add_argument('--local_dir', default=os.getcwd(), help='Directory to save training results of tune (default: %(default)s)')
     parser.add_argument('--num_samples', type=int, default=50, help='Number of running samples (default: %(default)s)')
     parser.add_argument('--mode', default='max', choices=['min', 'max'], help='Determines whether objective is minimizing or maximizing the metric attribute. (default: %(default)s)')
-    parser.add_argument('--search_alg', default=None, choices=['random', 'grid', 'bayesopt', 'optuna'], help='Search algorithms (default: %(default)s)')
+    parser.add_argument('--search_alg', default='basic_variant', choices=[
+                        'basic_variant', 'bayesopt', 'optuna'], help='Search algorithms (default: %(default)s)')
     args = parser.parse_args()
 
     """Other args in the model config are viewed as resolved values that are ignored from tune.
