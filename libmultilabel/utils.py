f938d584dd18a865561aadde073510b9c9ba3034
Henry Yang <Henry1_Yang@asus.com>
2021-06-12 18:18:56 +0000

    Replace ArgDict with AttributeDict

--- libmultilabel/utils.py
+++ libmultilabel/utils.py
@@ -8,31 +8,6 @@ import torch
 import numpy as np
 
 
-class ArgDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(ArgDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value."""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
 class Timer(object):
     """Computes elasped time."""
 
