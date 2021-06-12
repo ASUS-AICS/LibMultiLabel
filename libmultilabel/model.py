67de43fdd028fc3c22d9ef2627b1a9c0b888bbbf
Henry Yang <Henry1_Yang@asus.com>
2021-06-12 16:17:14 +0000

    Finish testing

--- libmultilabel/model.py
+++ libmultilabel/model.py
@@ -1,17 +1,11 @@
-import logging
-import os
-import shutil
-
 import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from tqdm import tqdm
 
-from . import data_utils, networks
-from .evaluate import MultiLabelMetrics, evaluate
-from .utils import AverageMeter, Timer, dump_log
+from . import networks
+from .evaluate import MultiLabelMetrics
 
 
 class Model(pl.LightningModule):
@@ -21,33 +15,19 @@ class Model(pl.LightningModule):
 
     def __init__(self, config, word_dict=None, classes=None, ckpt=None):
         super().__init__()
+        self.save_hyperparameters()
         self.config = config
-        self.patience = config.patience
 
-        if ckpt:
-            self.config.run_name = ckpt['run_name']
-            self.word_dict = ckpt['word_dict']
-            self.classes = ckpt['classes']
-            self.best_metric = ckpt['best_metric']
-            self.epoch = ckpt['epoch']
-        else:
-            self.word_dict = word_dict
-            self.classes = classes
-            self.epoch = 0
-            self.best_metric = 0
-
+        self.word_dict = word_dict
+        self.classes = classes
         self.config.num_classes = len(self.classes)
 
         embed_vecs = self.word_dict.vectors
         self.network = getattr(networks, config.model_name)(
             config, embed_vecs).to(config.device)
 
-        if ckpt:
-            self.network.load_state_dict(ckpt['state_dict'])
-            self.optimizer.load_state_dict(ckpt['optimizer'])
-        elif config.init_weight is not None:
-            init_weight = networks.get_init_weight_func(config)
-            self.apply(init_weight)
+        init_weight = networks.get_init_weight_func(config)
+        self.apply(init_weight)
 
     def configure_optimizers(self):
         """Initialize an optimizer for the free parameters of the network.
@@ -99,36 +79,8 @@ class Model(pl.LightningModule):
         self.log_dict(eval_metric.get_metric_dict())
         print(eval_metric)
 
-    def save(self, epoch, is_best=False):
-        self.network.eval()
-        ckpt = {
-            'epoch': epoch,
-            'run_name': self.config.run_name,
-            'state_dict': self.network.state_dict(),
-            'word_dict': self.word_dict,
-            'classes': self.classes,
-            'optimizer': self.optimizer.state_dict(),
-            'best_metric': self.best_metric,
-        }
-        ckpt_path = os.path.join(self.config.result_dir,
-                                 self.config.run_name, 'model_last.pt')
-        os.makedirs(os.path.dirname(ckpt_path), exist_ok=True)
-        logging.info(f"Save current  model: {ckpt_path}")
-        torch.save(ckpt, ckpt_path)
-        if is_best:
-            best_ckpt_path = ckpt_path.replace('last', 'best')
-            logging.info(
-                f"Save best model ({self.config.val_metric}: {self.best_metric}): {best_ckpt_path}")
-            shutil.copyfile(ckpt_path, best_ckpt_path)
-        self.network.train()
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
 
-    @staticmethod
-    def load(config, ckpt_path):
-        ckpt = torch.load(ckpt_path)
-        return Model(config, ckpt=ckpt)
-
-    def load_best(self):
-        best_ckpt_path = os.path.join(self.config.result_dir,
-                                      self.config.run_name, 'model_best.pt')
-        best_model = self.load(self.config, best_ckpt_path)
-        self.network = best_model.network
+    def test_epoch_end(self, test_step_outputs):
+        return self.validation_epoch_end(test_step_outputs)
