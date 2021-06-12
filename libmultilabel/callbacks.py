65e0191e9535649008906788ed60775716fc7f2b
Henry Yang <Henry1_Yang@asus.com>
2021-06-12 18:38:49 +0000

    Add EarlyStop to callbacks

---
+++ libmultilabel/callbacks.py
@@ -0,0 +1,20 @@
+import shutil
+
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+
+class EarlyStoppingWithCheckpoint(EarlyStopping):
+    def __init__(self, best_checkpoint_path, last_checkpoint_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.best_checkpoint_path = best_checkpoint_path
+        self.last_checkpoint_path = last_checkpoint_path
+        assert best_checkpoint_path and last_checkpoint_path, '`best_checkpoint_path` and `last_checkpoint_path` must be specified.'
+
+    def on_validation_end(self, trainer, pl_module):
+        ret = super().on_validation_end(trainer, pl_module)
+        trainer.save_checkpoint(self.last_checkpoint_path)
+        if self.wait_count == 0:  # best metric
+            shutil.copyfile(self.last_checkpoint_path,
+                            self.best_checkpoint_path)
+            print(f'Saved best model to `{self.best_checkpoint_path}`')
+        return ret
--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from pathlib import Path
 import os
 import yaml
 import pytorch_lightning as pl
+from pytorch_lightning.utilities.parsing import AttributeDict
 
-
+from libmultilabel.callbacks import EarlyStoppingWithCheckpoint
 from libmultilabel import data_utils
 from libmultilabel.model import Model
-from libmultilabel.utils import ArgDict, Timer, set_seed, init_device, dump_log, save_top_k_predictions
-from libmultilabel.evaluate import evaluate
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from libmultilabel.utils import Timer, set_seed, init_device, dump_log, save_top_k_predictions
 
 
 def get_config():
@@ -29,73 +28,114 @@ def get_config():
             config = yaml.load(fp, Loader=yaml.SafeLoader)
 
     # path / directory
-    parser.add_argument('--data_dir', default='./data/rcv1', help='The directory to load data (default: %(default)s)')
-    parser.add_argument('--result_dir', default='./runs', help='The directory to save checkpoints and logs (default: %(default)s)')
+    parser.add_argument('--data_dir', default='./data/rcv1',
+                        help='The directory to load data (default: %(default)s)')
+    parser.add_argument('--result_dir', default='./runs',
+                        help='The directory to save checkpoints and logs (default: %(default)s)')
 
     # data
-    parser.add_argument('--data_name', default='rcv1', help='Dataset name (default: %(default)s)')
-    parser.add_argument('--train_path', help='Path to training data (default: [data_dir]/train.txt)')
-    parser.add_argument('--val_path', help='Path to validation data (default: [data_dir]/valid.txt)')
-    parser.add_argument('--test_path', help='Path to test data (default: [data_dir]/test.txt)')
-    parser.add_argument('--val_size', type=float, default=0.2, help='Training-validation split: a ratio in [0, 1] or an integer for the size of the validation set (default: %(default)s).')
-    parser.add_argument('--min_vocab_freq', type=int, default=1, help='The minimum frequency needed to include a token in the vocabulary (default: %(default)s)')
-    parser.add_argument('--max_seq_length', type=int, default=500, help='The maximum number of tokens of a sample (default: %(default)s)')
-    parser.add_argument('--shuffle', type=bool, default=True, help='Whether to shuffle training data before each epoch (default: %(default)s)')
+    parser.add_argument('--data_name', default='rcv1',
+                        help='Dataset name (default: %(default)s)')
+    parser.add_argument(
+        '--train_path', help='Path to training data (default: [data_dir]/train.txt)')
+    parser.add_argument(
+        '--val_path', help='Path to validation data (default: [data_dir]/valid.txt)')
+    parser.add_argument(
+        '--test_path', help='Path to test data (default: [data_dir]/test.txt)')
+    parser.add_argument('--val_size', type=float, default=0.2,
+                        help='Training-validation split: a ratio in [0, 1] or an integer for the size of the validation set (default: %(default)s).')
+    parser.add_argument('--min_vocab_freq', type=int, default=1,
+                        help='The minimum frequency needed to include a token in the vocabulary (default: %(default)s)')
+    parser.add_argument('--max_seq_length', type=int, default=500,
+                        help='The maximum number of tokens of a sample (default: %(default)s)')
+    parser.add_argument('--shuffle', type=bool, default=True,
+                        help='Whether to shuffle training data before each epoch (default: %(default)s)')
 
     # train
-    parser.add_argument('--seed', type=int, help='Random seed (default: %(default)s)')
-    parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train (default: %(default)s)')
-    parser.add_argument('--batch_size', type=int, default=16, help='Size of training batches (default: %(default)s)')
-    parser.add_argument('--optimizer', default='adam', choices=['adam', 'sgd'], help='Optimizer: SGD or Adam (default: %(default)s)')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate for optimizer (default: %(default)s)')
-    parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay factor (default: %(default)s)')
-    parser.add_argument('--momentum', type=float, default=0.9, help='Momentum factor for SGD only (default: %(default)s)')
-    parser.add_argument('--patience', type=int, default=5, help='Number of epochs to wait for improvement before early stopping (default: %(default)s)')
+    parser.add_argument('--seed', type=int,
+                        help='Random seed (default: %(default)s)')
+    parser.add_argument('--epochs', type=int, default=10000,
+                        help='Number of epochs to train (default: %(default)s)')
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='Size of training batches (default: %(default)s)')
+    parser.add_argument('--optimizer', default='adam', choices=[
+                        'adam', 'sgd'], help='Optimizer: SGD or Adam (default: %(default)s)')
+    parser.add_argument('--learning_rate', type=float, default=0.0001,
+                        help='Learning rate for optimizer (default: %(default)s)')
+    parser.add_argument('--weight_decay', type=float, default=0,
+                        help='Weight decay factor (default: %(default)s)')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='Momentum factor for SGD only (default: %(default)s)')
+    parser.add_argument('--patience', type=int, default=5,
+                        help='Number of epochs to wait for improvement before early stopping (default: %(default)s)')
 
     # model
-    parser.add_argument('--model_name', default='KimCNN',help='Model to be used (default: %(default)s)')
-    parser.add_argument('--init_weight', default='kaiming_uniform', help='Weight initialization to be used (default: %(default)s)')
-    parser.add_argument('--activation', default='relu', help='Activation function to be used (default: %(default)s)')
-    parser.add_argument('--num_filter_per_size', type=int, default=128, help='Number of filters in convolutional layers in each size (default: %(default)s)')
-    parser.add_argument('--filter_sizes', type=int, nargs='+', default=[4], help='Size of convolutional filters (default: %(default)s)')
-    parser.add_argument('--dropout', type=float, default=0.2, help='Optional specification of dropout (default: %(default)s)')
-    parser.add_argument('--dropout2', type=float, default=0.2, help='Optional specification of the second dropout (default: %(default)s)')
-    parser.add_argument('--num_pool', type=int, default=1, help='Number of pool for dynamic max-pooling (default: %(default)s)')
+    parser.add_argument('--model_name', default='KimCNN',
+                        help='Model to be used (default: %(default)s)')
+    parser.add_argument('--init_weight', default='kaiming_uniform',
+                        help='Weight initialization to be used (default: %(default)s)')
+    parser.add_argument('--activation', default='relu',
+                        help='Activation function to be used (default: %(default)s)')
+    parser.add_argument('--num_filter_per_size', type=int, default=128,
+                        help='Number of filters in convolutional layers in each size (default: %(default)s)')
+    parser.add_argument('--filter_sizes', type=int, nargs='+',
+                        default=[4], help='Size of convolutional filters (default: %(default)s)')
+    parser.add_argument('--dropout', type=float, default=0.2,
+                        help='Optional specification of dropout (default: %(default)s)')
+    parser.add_argument('--dropout2', type=float, default=0.2,
+                        help='Optional specification of the second dropout (default: %(default)s)')
+    parser.add_argument('--num_pool', type=int, default=1,
+                        help='Number of pool for dynamic max-pooling (default: %(default)s)')
 
     # eval
-    parser.add_argument('--eval_batch_size', type=int, default=256, help='Size of evaluating batches (default: %(default)s)')
-    parser.add_argument('--metrics_thresholds', type=float, nargs='+', default=[0.5], help='Thresholds to monitor for metrics (default: %(default)s)')
-    parser.add_argument('--monitor_metrics', nargs='+', default=['P@1', 'P@3', 'P@5'], help='Metrics to monitor while validating (default: %(default)s)')
-    parser.add_argument('--val_metric', default='P@1', help='The metric to monitor for early stopping (default: %(default)s)')
+    parser.add_argument('--eval_batch_size', type=int, default=256,
+                        help='Size of evaluating batches (default: %(default)s)')
+    parser.add_argument('--metrics_thresholds', type=float, nargs='+', default=[
+                        0.5], help='Thresholds to monitor for metrics (default: %(default)s)')
+    parser.add_argument('--monitor_metrics', nargs='+', default=[
+                        'P@1', 'P@3', 'P@5'], help='Metrics to monitor while validating (default: %(default)s)')
+    parser.add_argument('--val_metric', default='P@1',
+                        help='The metric to monitor for early stopping (default: %(default)s)')
 
     # pretrained vocab / embeddings
-    parser.add_argument('--vocab_file', type=str, help='Path to a file holding vocabuaries (default: %(default)s)')
-    parser.add_argument('--embed_file', type=str, help='Path to a file holding pre-trained embeddings (default: %(default)s)')
-    parser.add_argument('--label_file', type=str, help='Path to a file holding all labels (default: %(default)s)')
+    parser.add_argument('--vocab_file', type=str,
+                        help='Path to a file holding vocabuaries (default: %(default)s)')
+    parser.add_argument('--embed_file', type=str,
+                        help='Path to a file holding pre-trained embeddings (default: %(default)s)')
+    parser.add_argument('--label_file', type=str,
+                        help='Path to a file holding all labels (default: %(default)s)')
 
     # log
-    parser.add_argument('--save_k_predictions', type=int, nargs='?', const=100, default=0, help='Save top k predictions on test set. k=%(const)s if not specified. (default: %(default)s)')
-    parser.add_argument('--predict_out_path', help='Path to the an output file holding top 100 label results (default: %(default)s)')
+    parser.add_argument('--save_k_predictions', type=int, nargs='?', const=100, default=0,
+                        help='Save top k predictions on test set. k=%(const)s if not specified. (default: %(default)s)')
+    parser.add_argument(
+        '--predict_out_path', help='Path to the an output file holding top 100 label results (default: %(default)s)')
 
     # others
     parser.add_argument('--cpu', action='store_true', help='Disable CUDA')
-    parser.add_argument('--silent', action='store_true', help='Enable silent mode')
-    parser.add_argument('--data_workers', type=int, default=4, help='Use multi-cpu core for data pre-processing (default: %(default)s)')
-    parser.add_argument('--embed_cache_dir', type=str, help='For parameter search only: path to a directory for storing embeddings for multiple runs. (default: %(default)s)')
-    parser.add_argument('--eval', action='store_true', help='Only run evaluation on the test set (default: %(default)s)')
-    parser.add_argument('--load_checkpoint', help='The checkpoint to warm-up with (default: %(default)s)')
+    parser.add_argument('--silent', action='store_true',
+                        help='Enable silent mode')
+    parser.add_argument('--data_workers', type=int, default=4,
+                        help='Use multi-cpu core for data pre-processing (default: %(default)s)')
+    parser.add_argument('--embed_cache_dir', type=str,
+                        help='For parameter search only: path to a directory for storing embeddings for multiple runs. (default: %(default)s)')
+    parser.add_argument('--eval', action='store_true',
+                        help='Only run evaluation on the test set (default: %(default)s)')
+    parser.add_argument(
+        '--checkpoint_path', help='The checkpoint to warm-up with (default: %(default)s)')
     parser.add_argument('-h', '--help', action='help')
 
     parser.set_defaults(**config)
     args = parser.parse_args()
-    config = ArgDict(vars(args))
+    config = AttributeDict(vars(args))
     return config
 
 
 def main():
     config = get_config()
     log_level = logging.WARNING if config.silent else logging.INFO
-    logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s:%(message)s')
+    logging.basicConfig(
+        level=log_level, format='%(asctime)s %(levelname)s:%(message)s')
     set_seed(seed=config.seed)
     config.device = init_device(use_cpu=config.cpu)
 
@@ -108,48 +148,51 @@ def main():
 
     datasets = data_utils.load_datasets(config)
 
+    checkpoint_dir = os.path.join(config.result_dir, config.run_name)
+    early_stop_callback = EarlyStoppingWithCheckpoint(
+        best_checkpoint_path=os.path.join(checkpoint_dir, 'best_model.ckpt'),
+        last_checkpoint_path=os.path.join(checkpoint_dir, 'lastest_model.ckpt'),
+        monitor=config.val_metric,
+        patience=config.patience,
+        verbose=True,
+        mode='max'
+    )
+
+    trainer = pl.Trainer(checkpoint_callback=False, logger=False,
+                         num_sanity_val_steps=0, gpus=1,
+                         max_epochs=config.epochs,
+                         callbacks=[early_stop_callback])
+
     if config.eval:
-        model = Model.load(config, config.load_checkpoint)
+        model = Model.load_from_checkpoint(config.checkpoint_path)
     else:
-        if config.load_checkpoint:
-            model = Model.load(config, config.load_checkpoint)
+        if config.checkpoint_path:
+            model = Model.load_from_checkpoint(config.checkpoint_path)
         else:
-            word_dict = data_utils.load_or_build_text_dict(config, datasets['train'])
+            word_dict = data_utils.load_or_build_text_dict(
+                config, datasets['train'])
             classes = data_utils.load_or_build_label(config, datasets)
             model = Model(config, word_dict, classes)
 
-        early_stop_callback = EarlyStopping(
-            monitor=config.val_metric,
-            patience=config.patience,
-            verbose=True,
-            mode='max'
-        )
-
-        trainer = pl.Trainer(checkpoint_callback=False, logger=False,
-                             num_sanity_val_steps=0, gpus=1,
-                             max_epochs=config.epochs,
-                             callbacks=[early_stop_callback])
-
         train_loader = data_utils.get_dataset_loader(
-            config, datasets['train'], word_dict, classes,
+            model.config, datasets['train'], model.word_dict, model.classes,
             shuffle=config.shuffle, train=True)
         val_loader = data_utils.get_dataset_loader(
-            config, datasets['val'], word_dict, classes, train=False)
+            model.config, datasets['val'], model.word_dict, model.classes, train=False)
 
         trainer.fit(model, train_loader, val_loader)
 
     if 'test' in datasets:
-        test_loader = data_utils.get_dataset_loader(config, datasets['test'], model.word_dict, model.classes, train=False)
-        test_metrics = evaluate(model, test_loader, config.monitor_metrics, silent=config.silent)
-        metric_dict = test_metrics.get_metric_dict(use_cache=False)
-        dump_log(config=config, metrics=metric_dict, split='test')
-        if not config.silent:
-            print(test_metrics)
-        if config.save_k_predictions > 0:
-            if not config.predict_out_path:
-                config.predict_out_path = os.path.join(config.result_dir, config.run_name, 'predictions.txt')
-            save_top_k_predictions(model.classes, test_metrics.get_y_pred(), config.predict_out_path, config.save_k_predictions)
+        test_loader = data_utils.get_dataset_loader(
+            model.config, datasets['test'], model.word_dict, model.classes, train=False)
+        test_results = trainer.test(model, test_dataloaders=test_loader, ckpt_path=os.path.join(checkpoint_dir, 'best_model.ckpt'))
 
+        # TODO add dump back
+        # dump_log(config=config, metrics=metric_dict, split='test')
+        # if config.save_k_predictions > 0:
+        #     if not config.predict_out_path:
+        #         config.predict_out_path = os.path.join(config.result_dir, config.run_name, 'predictions.txt')
+        #     save_top_k_predictions(model.classes, test_metrics.get_y_pred(), config.predict_out_path, config.save_k_predictions)
 
 if __name__ == '__main__':
     wall_time = Timer()
