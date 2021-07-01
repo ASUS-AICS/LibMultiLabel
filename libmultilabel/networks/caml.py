dcd4a0a22afa921565cc5fca6391a53ffd0f0f32
Eleven Liu <Eleven1_Liu@asus.com>
2021-07-01 15:08:43 +0000

    Add matrix shapes.

--- libmultilabel/networks/caml.py
+++ libmultilabel/networks/caml.py
@@ -17,15 +17,15 @@ class CAML(BaseModel):
 
         num_filter_per_size = config.num_filter_per_size
 
-        # initialize conv layer as in 2.1
+        # Initialize conv layer
         self.conv = nn.Conv1d(embed_vecs.shape[1], num_filter_per_size, kernel_size=filter_size, padding=int(floor(filter_size/2)))
         xavier_uniform_(self.conv.weight)
 
-        # Context vectors for computing attention as in 2.2
+        # Context vectors for computing attention
         self.U = nn.Linear(num_filter_per_size, config.num_classes)
         xavier_uniform_(self.U.weight)
 
-        # Final layer: create a matrix to use for the L binary classifiers as in 2.3
+        # Final layer: create a matrix to use for the L binary classifiers
         self.final = nn.Linear(num_filter_per_size, config.num_classes)
         xavier_uniform_(self.final.weight)
 
@@ -35,19 +35,20 @@ class CAML(BaseModel):
         x = self.embed_drop(x)
         x = x.transpose(1,2)
 
-        # Apply convolution and nonlinearity (tanh / relu)
+        # Apply convolution and nonlinearity (tanh)
+        # batch_size * document length * num_filter_per_size
         x = torch.tanh(self.conv(x).transpose(1,2))
 
         # Apply attention
-        #    batch * text_length * 1200
-        # -> batch * text_length * num_class (matmul)
-        # -> batch * num_class (softmax)
+        # batch_size * class num * document length
         alpha = torch.softmax(self.U.weight.matmul(x.transpose(1,2)), dim=2)
 
-        # Document representations are weighted sums using the attention. Can compute all at once as a matmul
+        # Document representations are weighted sums using the attention.
+        # batch_size * class num * num_filter_per_size
         m = alpha.matmul(x)
 
-        # similarity
+        # Similarity
+        # batch_size * class num
         x = self.final.weight.mul(m).sum(dim=2).add(self.final.bias)
 
         return {'logits': x, 'attention': alpha}
