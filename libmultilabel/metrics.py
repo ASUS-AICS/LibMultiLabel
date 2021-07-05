96084b4ed6a06861777f45d610b2d61ac685b8cb
Eleven Liu <Eleven1_Liu@asus.com>
2021-07-05 03:13:40 +0000

    sort p@k

--- libmultilabel/metrics.py
+++ libmultilabel/metrics.py
@@ -73,7 +73,7 @@ class MultiLabelMetrics():
         }
         # add metrics like P@k, R@k to the result dict
         scores = precision_recall_at_ks(y_true, y_pred, top_ks=self.top_ks)
-        precision_recall_monitored = set(self.monitor_metrics) & scores.keys()
+        precision_recall_monitored = sorted(set(self.monitor_metrics) & scores.keys())
         result.update({metric: scores[metric] for metric in precision_recall_monitored})
         self.cached_results = result
 
