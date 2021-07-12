0662fb0f30cebf65b4c8b8e63474d20cbd3ca2ac
Eleven Liu <Eleven1_Liu@asus.com>
2021-07-12 07:31:38 +0000

    Also update vanilla_cnn.

--- libmultilabel/networks/vanilla_cnn.py
+++ libmultilabel/networks/vanilla_cnn.py
@@ -7,19 +7,25 @@ from ..networks.base import BaseModel
 
 
 class VanillaCNN(BaseModel):
-    def __init__(self, config, embed_vecs):
-        super(VanillaCNN, self).__init__(config, embed_vecs)
+    def __init__(
+        self,
+        embed_vecs,
+        num_classes,
+        filter_sizes=None,
+        num_filter_per_size=500,
+        **kwargs
+    ):
+        super(VanillaCNN, self).__init__(embed_vecs, **kwargs)
+        if len(filter_sizes) != 1:
+            raise ValueError(f'VanillaCNN expect 1 filter size. Got filter_sizes={filter_sizes}')
+        filter_size = filter_sizes[0]
 
-        if len(config.filter_sizes) != 1:
-            raise ValueError(f'VanillaCNN expect 1 filter size. Got filter_sizes={config.filter_sizes}')
-        filter_size = config.filter_sizes[0]
-
-        num_filter_per_size = config.num_filter_per_size
+        num_filter_per_size = num_filter_per_size
 
         self.conv = nn.Conv1d(embed_vecs.shape[1], num_filter_per_size, kernel_size=filter_size)
         xavier_uniform_(self.conv.weight)
 
-        self.linear = nn.Linear(num_filter_per_size, config.num_classes)
+        self.linear = nn.Linear(num_filter_per_size, num_classes)
         xavier_uniform_(self.linear.weight)
 
 
