d0d3ec5c541a30a5ca36154962a1887a83124384
Si-An Chen <sian1_chen@asus.com>
2021-07-16 07:26:02 +0000

    move to metric.py and allow customized metrics

--- libmultilabel/model.py
+++ libmultilabel/model.py
@@ -1,4 +1,3 @@
-import re
 from abc import abstractmethod
 from argparse import Namespace
 
@@ -7,10 +6,10 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from torchmetrics import MetricCollection, F1, Precision, Recall
 
 from . import networks
 from .utils import dump_log, argsort_top_k
+from .metrics import get_metrics, tabulate_metrics
 
 
 class MultiLabelModel(pl.LightningModule):
@@ -42,29 +41,9 @@ class MultiLabelModel(pl.LightningModule):
         self.silent = silent
         self.save_k_predictions = save_k_predictions
 
-        macro_prec = Precision(self.num_classes, metric_threshold, average='macro')
-        macro_recall = Recall(self.num_classes, metric_threshold, average='macro')
-        another_macro_f1 = 2 * (macro_prec * macro_recall) / (macro_prec + macro_recall + 1e-10)
-        metrics = {
-            'Micro-Precision': Precision(self.num_classes, metric_threshold, average='micro'),
-            'Micro-Recall': Recall(self.num_classes, metric_threshold, average='micro'),
-            'Micro-F1': F1(self.num_classes, metric_threshold, average='micro'),
-            'Macro-F1': F1(self.num_classes, metric_threshold, average='macro'),
-            # The f1 value of macro_precision and macro_recall. This variant of
-            # macro_f1 is less preferred but is used in some works. Please
-            # refer to Opitz et al. 2019 [https://arxiv.org/pdf/1911.03347.pdf]
-            'Another-Macro-F1': another_macro_f1,
-        }
-        for metric in monitor_metrics:
-            if re.match('P@\d+', metric):
-                metrics[metric] = Precision(self.num_classes, metric_threshold,
-                                            average='samples', top_k=int(metric[2:]))
-            elif re.match('R@\d+', metric):
-                metrics[metric] = Recall(self.num_classes, metric_threshold,
-                                         average='samples', top_k=int(metric[2:]))
-            elif metric not in ['Micro-Precision', 'Micro-Recall', 'Micro-F1', 'Macro-F1', 'Another-Macro-F1']:
-                raise ValueError(f'Invalid metric: {metric}')
-        self.eval_metric = MetricCollection(metrics)
+        # metrics for evaluation
+        self.eval_metric = get_metrics(metric_threshold, monitor_metrics,
+                                       self.num_classes)
 
     def configure_optimizers(self):
         """Initialize an optimizer for the free parameters of the network.
@@ -133,12 +112,7 @@ class MultiLabelModel(pl.LightningModule):
         for k, v in metric_dict.items():
             metric_dict[k] = v.item()
         dump_log(metrics=metric_dict, split=split, log_path=self.log_path)
-
-        if not self.silent and (not self.trainer or self.trainer.is_global_zero):
-            self.print(f'====== {split} dataset evaluation result =======')
-            header = '|'.join([f'{k:^18}' for k in metric_dict.keys()])
-            values = '|'.join([f'{x * 100:^18.4f}' if isinstance(x, (np.floating, float)) else f'{x:^18}' for x in metric_dict.values()])
-            self.print(f"|{header}|\n|{'-----------------:|' * len(metric_dict)}\n|{values}|\n")
+        self.print(tabulate_metrics(metric_dict, split))
         self.eval_metric.reset()
         return metric_dict
 
