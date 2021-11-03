24dd12d0e36105c84bc4a4460a6123478dbb3fb0
Eleven Liu <eleven1_liu@asus.com>
2021-11-03 06:49:35 +0000

    Make clear the comment.

--- torch_trainer.py
+++ torch_trainer.py
@@ -134,8 +134,8 @@ class TorchTrainer:
             val_loader = self._get_dataset_loader(split='val')
             self.trainer.fit(self.model, train_loader, val_loader)
 
-        # Set model to the best model. If the val loop is skipped during training (i.e., val_size=0),
-        # the model is set to the last model.
+        # Set model to the best model. If the validation process is skipped during
+        # training (i.e., val_size=0), the model is set to the last model.
         checkpoint_callback = [callback for callback in self.trainer.callbacks if isinstance(callback, ModelCheckpoint)][0]
         model_path = checkpoint_callback.best_model_path if checkpoint_callback.best_model_path else checkpoint_callback.last_model_path
         logging.info(f'Finished training. Load best model from {model_path}.')
