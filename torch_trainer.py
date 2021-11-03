dd661e635b400f7b5e9f00408261a870a5ef19d0
Eleven Liu <eleven1_liu@asus.com>
2021-11-03 16:31:44 +0800

    Replace if..else to or.
    Co-authored-by: sianchen <sian1_chen@asus.com>

--- torch_trainer.py
+++ torch_trainer.py
@@ -137,7 +137,7 @@ class TorchTrainer:
         # Set model to the best model. If the validation process is skipped during
         # training (i.e., val_size=0), the model is set to the last model.
         checkpoint_callback = [callback for callback in self.trainer.callbacks if isinstance(callback, ModelCheckpoint)][0]
-        model_path = checkpoint_callback.best_model_path if checkpoint_callback.best_model_path else checkpoint_callback.last_model_path
+        model_path = checkpoint_callback.best_model_path or checkpoint_callback.last_model_path
         logging.info(f'Finished training. Load best model from {model_path}.')
         self._setup_model(checkpoint_path=model_path)
 
