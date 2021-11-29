3557048c2a4f1753ae06d9e78cd8a60656305f17
Eleven Liu <eleven1_liu@asus.com>
2021-11-29 04:30:47 +0000

    test on the testing dataset in the end

--- search_params.py
+++ search_params.py
@@ -179,7 +179,7 @@ def retrain_best_model(log_path, merge_train_val=False):
     trainer = TorchTrainer(config=best_config, **data)
     trainer.train()
 
-    if 'test' in data:
+    if 'test' in data['datasets']:
         test_results = trainer.test()
         logging.info(f'Test results after retraining: {test_results}')
     logging.info(f'Best model saved to {trainer.checkpoint_callback.best_model_path or trainer.checkpoint_callback.last_model_path}.')
