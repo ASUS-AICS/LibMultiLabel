9df1f3ce6cae128eed57f1077f98f972f8276bef
Sinacam <stinkingmadgod@gmail.com>
2022-02-14 10:44:02 +0800

    initial tree implementation

--- libmultilabel/linear/tree.py
+++ libmultilabel/linear/tree.py
@@ -1,8 +1,11 @@
 import numpy as np
 import scipy.sparse as sparse
 
+<<<<<<< HEAD
 import time
 
+=======
+>>>>>>> initial tree implementation
 from sklearn.cluster import KMeans
 
 from .linear import train_1vsrest, predict_values
@@ -11,103 +14,91 @@ from .linear import train_1vsrest, predict_values
 class Node:
     def __init__(self,
                  labelmap: np.ndarray,
-                 children: 'list[Node]',
-                 metalabels: np.ndarray,
+                 children: 'list[Node]'
                  ) -> None:
         self.labelmap = labelmap
         self.children = children
-        self.metalabels = metalabels
 
-    def isLeaf(self) -> bool:
-        return len(self.children) == 0
-
-    def dfs(self, visit):
-        visit(self)
-        for child in self.children:
-            child.dfs(visit)
-
-ovrtime = 0
 
 class Tree:
-    def __init__(self) -> None:
+    def __init__(self,
+                 y: sparse.csr_matrix,
+                 x: sparse.csr_matrix
+                 ) -> None:
         self.K = 100
         self.dmax = 10   # no mention of value?
+        rep = y.T * sparse.hstack([x, y])
+        self.root = self._build(rep, np.arange(y.shape[0]), 0)
         self.beam_width = 10    # no mention of value?
-
-    def train(self,
-              y: sparse.csr_matrix,
-              x: sparse.csr_matrix,
-              options: str
-              ) -> None:
-        rep = (y.T * x).tocsr()
-
-        start = time.time()
-        self.root = self._build(rep, np.arange(y.shape[1]), 0)
-        print(f'tree building done in {time.time() - start}s')
-
-        def visit(node):
-            idx = y[:, node.labelmap].getnnz(axis=1) > 0
-            return self._train_node(y[idx], x[idx], options, node)
-
-        start = time.time()
-        self.root.dfs(visit)
-        print(f'training done in {time.time() - start}s')
 
     def _build(self,
                rep: sparse.csr_matrix,
                labelmap: np.ndarray,
                d: int
                ) -> Node:
-        if d >= self.dmax or rep.shape[0] < self.K:
-            return Node(labelmap, [], np.arange(len(labelmap)))
+        if d >= self.dmax or rep.shape[0] >= self.K:
+            return Node(labelmap, [])
 
-        metalabels = KMeans(self.K).fit(rep).labels_
-        # metalabels = KMeans(self.K, n_init=1, tol=1e-3).fit(rep).labels_
-        maps = [labelmap[metalabels == i] for i in range(self.K)]
-        reps = [rep[metalabels == i] for i in range(self.K)]
-        children = [self._build(reps[i], maps[i], d+1)
+        clusters = KMeans(self.K).fit(rep).labels_
+        labelmaps = [labelmap[clusters == i] for i in range(self.K)]
+        reps = [rep[clusters == i] for i in range(self.K)]
+        children = [self._build(reps[i], labelmaps[i], d+1)
                     for i in range(self.K)]
-        return Node(labelmap, children, metalabels)
+        return Node(labelmap, children)
+
+    def train(self,
+              y: sparse.csr_matrix,
+              x: sparse.csr_matrix,
+              options: str
+              ) -> None:
+        self._dfs(
+            self.root,
+            lambda node: self._train_node(y, x, options, node),
+        )
 
     @staticmethod
     def _train_node(y: sparse.csr_matrix,
                     x: sparse.csr_matrix,
                     options: str,
                     node: Node
-                    ):
-        global ovrtime
-        if node.isLeaf():
-            start = time.time()
+                    ) -> bool:
+        if node.children == []:
             node.model = train_1vsrest(
                 y[:, node.labelmap], x, options,
             )
-            ovrtime += time.time() - start
         else:
-            childy = [np.sum(y[:, child.labelmap], axis=1).reshape(-1, 1) > 0
+            childy = [np.sum(y[:, child.labelmap], axis=0).reshape(-1, 1) > 0
                       for child in node.children]
-            childy = sparse.csr_matrix(np.hstack(childy))
-            start = time.time()
+            childy = sparse.hstack(childy).tocsr()
             node.model = train_1vsrest(
                 childy, x, options,
             )
-            ovrtime += time.time() - start
+        return True
+
+    def _dfs(self, node, visit):
+        if not visit(node):
+            return
+        for child in node.children:
+            self._dfs(child, visit)
 
     def predict_values(self, x: sparse.csr_matrix) -> np.ndarray:
-        num_class = self.root.labelmap.shape[0]
-        totalprob = np.ones((x.shape[0], num_class))
-        self._beam_search(totalprob, x, self.root)
-        return totalprob
+        self._beam_search(x, self.root)
 
-    def _beam_search(self, totalprob: np.ndarray, x: sparse.csr_matrix, node: Node):
+    def _beam_search(self, x: sparse.csr_matrix, node: Node) -> np.ndarray:
+        num_class = self.root.labelmap.shape[0]
+
+        ret = np.zeros((x.shape[0], num_class))
         pred = predict_values(node.model, x)
-        prob = 1 / (1 + np.exp(-pred))
-        if node.isLeaf():
-            totalprob[:, node.labelmap] *= prob
-        else:
-            totalprob[:, node.labelmap] *= prob[:, node.metalabels]
-            top = np.argpartition(pred, -self.beam_width,
-                                  axis=1)[:, -self.beam_width:]
-            for i, child in enumerate(node.children):
-                possible = np.sum(top == i, axis=1) > 0
-                self._beam_search(totalprob[possible], x[possible], child)
-                totalprob[np.ix_(~possible, child.labelmap)] = 0
+        ret[node.labelmap] = pred
+
+        if node.children != []:
+            ord = np.argsort(pred, axis=1)
+            top = ord[:, -self.beam_width:]
+            childidx = [np.where(top == i)[0]
+                        for i in range(len(node.children))]
+            childpred = [self._beam_search(x[childidx], child)
+                         for child in node.children]
+            for ci, cp in zip(childidx, childpred):
+                ret[ci] = ret[ci] * cp
+
+        return ret
