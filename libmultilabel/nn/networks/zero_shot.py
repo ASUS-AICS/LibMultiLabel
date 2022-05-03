d5753600f97fdbac340310e2e12e646e6836f8d0
Si-An <d09922007@csie.ntu.edu.tw>
2022-05-03 18:29:48 +0800

    zero-shot bigru inner&linear

--- libmultilabel/nn/networks/zero_shot.py
+++ libmultilabel/nn/networks/zero_shot.py
@@ -4,29 +4,50 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .modules import Embedding, GRUEncoder, LSTMEncoder, CNNEncoder, LabelwiseAttention, LabelwiseMultiHeadAttention, LabelwiseLinearOutput
+from .modules import Embedding, GRUEncoder
 
 
-class CBiGRULWAN(nn.Module):
+class ZeroBiGRU(nn.Module):
 
-    def __init__(self, embed_vecs, num_classes, embed_dropout, encoder_dropout, rnn_dim, rnn_layers, label_embedding):
-        super(CBiGRULWAN, self).__init__()
+    def __init__(self, embed_vecs, num_classes, embed_dropout, encoder_dropout,
+                 rnn_dim, rnn_layers, label_embedding, output_type):
+        super().__init__()
+        embed_dim = embed_vecs.shape[1]
         self.embedding = Embedding(embed_vecs, embed_dropout)
         assert rnn_dim % 2 == 0, """`rnn_dim` should be even."""
-        self.encoder = GRUEncoder(embed_vecs.shape[1], rnn_dim // 2, rnn_layers, encoder_dropout)
-        self.linear = nn.Linear(rnn_dim, embed_vecs.shape[1])
+        self.encoder = GRUEncoder(embed_dim, rnn_dim // 2, rnn_layers, encoder_dropout)
         self.Q = nn.Parameter(label_embedding, requires_grad=False)
-        self.output = LabelProduct(label_embedding)
+        self.attention = LabelAttention(rnn_dim, embed_dim)
+        self.output_type = output_type
+        if output_type == 'inner':
+            self.output = SharedLinearLabelProduct(rnn_dim, embed_dim)
+        elif output_type == 'linear':
+            self.output = SharedLinear(rnn_dim)
+        else:
+            raise ValueError(f'Invalid output type: {output_type}')
 
     def forward(self, input):
         x = self.embedding(input['text'])  # (batch_size, sequence_length, embed_dim)
         x = self.encoder(x, input['length'])  # (batch_size, sequence_length, hidden_dim)
-        V = torch.tanh(self.linear(x))  # (batch_size, sequence_length, hidden_dim)
-        A = F.softmax(torch.matmul(self.Q, V.transpose(1, 2)), -1) # (batch_size, num_classes, sequence_length)
-        E = torch.bmm(A, x)  # (batch_size, num_classes, hidden_dim)
-        # x = (self.Q * E).sum(-1) # (batch_size, num_classes)
-        x = self.output(E)
+        x = self.attention(x, self.Q) # (batch_size, num_classes, hidden_dim)
+        if self.output_type == 'inner':
+            x = self.output(x, self.Q)
+        else:
+            x = self.output(x)
         return {'logits': x}
+
+
+class LabelAttention(nn.Module):
+
+    def __init__(self, input_size, embed_dim):
+        super().__init__()
+        self.linear = nn.Linear(input_size, embed_dim)
+
+    def forward(self, x, Q):
+        V = torch.tanh(self.linear(x))  # (batch_size, sequence_length, embed_dim)
+        A = F.softmax(torch.matmul(Q, V.transpose(1, 2)), -1) # (batch_size, num_classes, sequence_length)
+        E = torch.bmm(A, x)  # (batch_size, num_classes, hidden_dim)
+        return E
 
 
 class LabelProduct(nn.Module):
@@ -48,12 +69,10 @@ class SharedLinear(nn.Module):
 
 
 class SharedLinearLabelProduct(nn.Module):
-    def __init__(self, input_dim, label_embedding):
+    def __init__(self, input_dim, embed_dim):
         super().__init__()
-        self.Q = nn.Parameter(label_embedding, requires_grad=False)
-        output_dim = label_embedding.shape[-1]
-        self.linear = nn.Linear(input_dim, output_dim)
+        self.linear = nn.Linear(input_dim, embed_dim)
 
-    def forward(self, x):
+    def forward(self, x, Q):
         x = self.linear(x)
-        return (self.Q * x).sum(-1)
+        return (Q * x).sum(-1)
