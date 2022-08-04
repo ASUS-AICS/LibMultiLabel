930bd549ea65a49f8e9ae1958a7bf091cafb56b3
Sinacam <stinkingmadgod@gmail.com>
2022-08-04 21:47:40 +0800

    cleaning up

--- libmultilabel/linear/linear.py
+++ libmultilabel/linear/linear.py
@@ -1,10 +1,10 @@
+import logging
 import os
 
-import logging
 import numpy as np
 import scipy.sparse as sparse
-from tqdm import tqdm
 from liblinear.liblinearutil import train
+from tqdm import tqdm
 
 __all__ = ['train_1vsrest',
            'train_thresholding',
@@ -12,8 +12,6 @@ __all__ = ['train_1vsrest',
            'train_cost_sensitive_micro',
            'predict_values']
 
-def training_log(technique, num_class):
-    return f'Training {technique} model on {num_class} labels'
 
 def train_1vsrest(y: sparse.csr_matrix, x: sparse.csr_matrix, options: str):
     """Trains a linear model for multiabel data using a one-vs-rest strategy.
@@ -347,7 +345,8 @@ def train_cost_sensitive(y: sparse.csr_matrix, x: sparse.csr_matrix, options: st
     num_feature = x.shape[1]
     weights = np.zeros((num_feature, num_class), order='F')
 
-    logging.info(f'Training cost-sensitive model for Macro-F1 on {num_class} labels')
+    logging.info(
+        f'Training cost-sensitive model for Macro-F1 on {num_class} labels')
     for i in tqdm(range(num_class)):
         yi = y[:, i].toarray().reshape(-1)
         w = cost_sensitive_one_label(2*yi - 1, x, options)
@@ -448,7 +447,8 @@ def train_cost_sensitive_micro(y: sparse.csr_matrix, x: sparse.csr_matrix, optio
     param_space = [1, 1.33, 1.8, 2.5, 3.67, 6, 13]
     bestScore = -np.Inf
 
-    logging.info(f'Training cost-sensitive model for Micro-F1 on {num_class} labels')
+    logging.info(
+        f'Training cost-sensitive model for Micro-F1 on {num_class} labels')
     for a in param_space:
         tp = fn = fp = 0
         for i in tqdm(range(num_class)):
