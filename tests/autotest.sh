5467e15128d6eab1e5a85cefa82f154432255256
JamesLYC88 <ycl880326@gmail.com>
2022-08-10 14:15:42 +0000

    add component test

--- tests/autotest.sh
+++ tests/autotest.sh
@@ -27,7 +27,7 @@ get_test_results() {
 #######################################
 # Test results between current branch and master branch.
 # Arguments:
-#   $1: Data name such as MIMIC-50 or rcv1.
+#   $1: Data name such as EUR-Lex, MIMIC-50, or rcv1.
 #   $2: Nework name defined in libmultilabel/nn/networks/*
 #   $3: Command template to run.
 #######################################
@@ -35,6 +35,7 @@ run_test() {
   data_name=$1
   network_name=$2
   command=$(echo "$3" | sed "s/%s/$data_name/" | sed "s/%s/$network_name/")
+  echo "Testing command: $command" >> $REPORT_PATH
 
   declare -A results actual_results
   prefix="${RESULT_DIR}/${data_name}_${network_name}_"
@@ -43,14 +44,25 @@ run_test() {
   get_test_results $prefix "$command"
   for i in "${!results[@]}"; do actual_results[$i]=${results[$i]}; done
 
+  # Record the checkpoint directory for current branch
+  current_dir=$(ls -t $prefix*[0-9]*/logs.json | head -1 | sed "s/logs.json//")
+
   # Get expected results in master branch
   git checkout master
   get_test_results $prefix "$command"
 
+  # Record the checkpoint directory for master branch
+  master_dir=$(ls -t $prefix*[0-9]*/logs.json | head -1 | sed "s/logs.json//")
+
+  # Compare each API component
+  if [ "$network_name" != "l2svm" ]; then
+    python3 tests/compare_api.py --current_dir $current_dir --master_dir $master_dir >> $REPORT_PATH
+  fi
+
   # Compare the results between current branch and master.
   cmp=$(echo ${results[@]} ${actual_results[@]} | tr ' ' '\n' | sort | uniq -u | wc -l)
   is_passed=$(echo $cmp | grep -q "0" && echo "PASSED" || echo "FAILED")
-  echo "Test $is_passed!" & echo "$is_passed   $command" >> $REPORT_PATH &
+  echo "Test $is_passed!" & echo "results $is_passed" >> $REPORT_PATH &
 
   echo "Switch to $BRANCH_TO_TEST ..."
   git checkout $BRANCH_TO_TEST # back to current branch
@@ -63,11 +75,11 @@ main() {
 
   TEST_COMMAND_TEMPLATES=(
     # Run 20% of the training data, 20% of the validation data, and 1% of the test data for 2 epochs.
-    "python3 main.py --config example_config/%s/%s.yml --result_dir $RESULT_DIR --limit_train_batches 0.2 --limit_val_batches 0.2 --limit_test_batches 0.01 --epochs 2"
+    "python3 test_main.py --config example_config/%s/%s.yml --result_dir $RESULT_DIR --limit_train_batches 0.2 --limit_val_batches 0.2 --limit_test_batches 0.01 --epochs 2"
   )
   for template in "${TEST_COMMAND_TEMPLATES[@]}"; do
+    run_test "EUR-Lex" "kim_cnn" "$template"
     run_test "MIMIC-50" "caml" "$template"
-    run_test "rcv1" "kim_cnn" "$template"
   done
 
   TEST_COMMAND_TEMPLATES=(
@@ -79,7 +91,7 @@ main() {
   done
 
   # Print the test results and remove the intermediate files.
-  all_tests=$(less $REPORT_PATH | wc -l)
+  all_tests=$(grep 'PASSED\|FAILED' $REPORT_PATH | wc -l)
   passed_tests=$(grep "PASSED" $REPORT_PATH | wc -l)
   echo "All tests finished ($passed_tests/$all_tests) on $BRANCH_TO_TEST. See $REPORT_PATH for details."
   rm -r $RESULT_DIR
---
+++ tests/nn/components.py
@@ -0,0 +1,71 @@
+class datasets(object):
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return 'datasets'
+
+    def get_from_trainer(self, trainer):
+        return trainer.datasets
+
+    def compare(self, a, b):
+        return a == b
+
+
+class stoi(object):
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return 'stoi'
+
+    def get_from_trainer(self, trainer):
+        return trainer.model.word_dict.get_stoi()
+
+    def compare(self, a, b):
+        return a == b
+
+
+class embed_vecs(object):
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return 'embed_vecs'
+
+    def get_from_trainer(self, trainer):
+        return trainer.model.embed_vecs
+
+    def compare(self, a, b):
+        return (a == b).all()
+
+
+class classes(object):
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return 'classes'
+
+    def get_from_trainer(self, trainer):
+        return trainer.model.classes
+
+    def compare(self, a, b):
+        return a == b
+
+
+class network(object):
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return 'network'
+
+    def get_from_trainer(self, trainer):
+        return trainer.model.network.state_dict()
+
+    def compare(self, a, b):
+        for aa, bb in zip(a, b):
+            if not (aa == bb):
+                return False
+        return True
