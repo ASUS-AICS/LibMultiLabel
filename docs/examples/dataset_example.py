6557c70178431e7154278447a7c1b987acdbe08c
Sheng-Wei Chen <will945945945@gmail.com>
2022-11-24 15:45:32 +0800

    update example code

--- docs/examples/dataset_example.py
+++ docs/examples/dataset_example.py
@@ -20,3 +20,60 @@ for tag in ['train', 'val']:
     num_no_label_data = sum(1 for d in data[tag] if len(d['label']) == 0)
     if num_no_label_data > 0:
         data[tag] = [d for d in data[tag] if len(d['label']) > 0]
+
+# Using LibMultiLabel with the datasets
+from libmultilabel.nn.data_utils import *
+from libmultilabel.nn.nn_utils import *
+
+# Setup device.
+set_seed(1337)
+device = init_device()  # use gpu by default
+
+# Preprocessing the datasets.
+classes = load_or_build_label(data)
+word_dict, embed_vecs = load_or_build_text_dict(dataset=data['train'], embed_file='glove.6B.300d')
+tokenizer = None
+add_special_tokens = False
+
+# Initialize a model.
+model_name = 'KimCNN'
+network_config = {
+    'embed_dropout': 0.2,
+    'encoder_dropout': 0.2,
+    'filter_sizes': [2, 4, 8],
+    'num_filter_per_size': 128
+}
+learning_rate = 0.0001
+model = init_model(
+    model_name=model_name,
+    network_config=network_config,
+    classes=classes,
+    word_dict=word_dict,
+    embed_vecs=embed_vecs,
+    learning_rate=learning_rate,
+    monitor_metrics=['Micro-F1', 'Macro-F1', 'P@1']
+)
+
+# Initialize a trainer.
+trainer = init_trainer(checkpoint_dir='runs/NN-example', epochs=15, val_metric='P@1')
+
+# Create data loaders.
+loaders = dict()
+for split in ['train', 'val', 'test']:
+    loaders[split] = get_dataset_loader(
+        data=data[split],
+        word_dict=word_dict,
+        classes=classes,
+        device=device,
+        max_seq_length=512,
+        batch_size=8,
+        shuffle=True if split == 'train' else False,
+        tokenizer=tokenizer,
+        add_special_tokens=add_special_tokens
+    )
+
+# Train a model from scratch.
+trainer.fit(model, loaders['train'], loaders['val'])
+
+# Test the model.
+trainer.test(model, dataloaders=loaders['test'])
