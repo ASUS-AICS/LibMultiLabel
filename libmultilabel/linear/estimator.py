838a46b790f6407a3b3cb555429e49dae14f366b
Eleven Liu <jiejyunliu@gmail.com>
2022-12-25 15:04:57 +0800

    Add multiLabel classifier and MultiLabelMixin.

--- libmultilabel/linear/estimator.py
+++ libmultilabel/linear/estimator.py
@@ -1,29 +1,50 @@
-import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.base import BaseEstimator
+from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 
-from libmultilabel.linear import predict_values, train_binary_and_multiclass
+import libmultilabel.linear as linear
 
 
-class MulticlassClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, options):
+class MultiLabelMixin:
+    """Mixin class for all classifiers in scikit-learn."""
+
+    _estimator_type = "classifier"
+
+    def __init__(self, metric_threshold=0, val_metric="P@1"):
+        self.metrics = None
+        self.metric_threshold = metric_threshold
+        self.val_metric = val_metric
+
+    def score(self, X, y):
+        if self.metrics is None:
+            self.metrics = linear.get_metrics(
+                self.metric_threshold,
+                [self.val_metric],
+                y.shape[1],
+            )
+        preds = self.predict(X)
+        target = y.toarray() # datasets['test']['y'][slice].toarray()
+        self.metrics.update(preds, target)
+        metric_dict = self.metrics.compute()
+        return metric_dict[self.val_metric]
+
+    def _more_tags(self):
+        return {"requires_y": True}
+
+
+class MultiLabelClassifier(MultiLabelMixin, BaseEstimator):
+    def __init__(self, options, metric_threshold=0, val_metric="P@1"):
+        super().__init__(metric_threshold, val_metric)
         self.options = options
 
     def fit(self, X, y):
-        # We need to validate the data because we do a safe_indexing later.
-        # X, y = self._validate_data(
-        #     X, y, accept_sparse=["csr", "csc"], force_all_finite=False
-        # )
         # Store the classes seen during fit
         self.classes_ = unique_labels(y)
-
-        # Return the classifier
-        self.model = train_binary_and_multiclass(y, X, self.options)
+        self.model = linear.train_1vsrest(y, X, self.options)
         return self
 
     def predict(self, X):
         # Check if fit has been called
         check_is_fitted(self)
-        preds = predict_values(self.model, X)
-        return np.argmax(preds, axis=1)
+        preds = linear.predict_values(self.model, X)
+        return preds
