53ec28373de60eb75da8fd292214282e35aed470
sammer1107 <qscesz871107@gmail.com>
2023-01-10 11:04:48 +0800

    add comments explaining how to collect logs
    + use double quotes for comments

--- libmultilabel/logging.py
+++ libmultilabel/logging.py
@@ -5,9 +5,8 @@ LOG_FORMAT = '%(asctime)s %(levelname)s:%(message)s'
 
 
 class ListHandler(logging.Handler):
-    """Collect logged message to a list of string
-       that can be obtained later.
-    """
+    """Collect logged message to a list of string that can be obtained later."""
+
     def __init__(self, level=logging.NOTSET):
         super().__init__(level)
         self.logs = []
@@ -17,11 +16,11 @@ class ListHandler(logging.Handler):
         self.logs.append(string)
 
     def get_logs(self):
-        '''Return and clear all the collected logs.
+        """Return and clear all the collected logs.
 
         Returns:
             list[str]: A list of formatted logs.
-        '''
+        """
         logs = self.logs
         self.logs = []
         return logs
@@ -29,16 +28,16 @@ class ListHandler(logging.Handler):
 stream_handler = None
 
 def add_stream_handler(level=logging.INFO):
-    '''Create and return a stream handler so that logging messages is
-        send to the terminal. The stream handler is attached to the root logger.
-        The logging messages from the ``transformer'' and ``pytorch_lighting''
-        module set to propagate to the root logger.
-        If the handler had been created, this function returns the handler
-        created ealier instead.
+    """Create and return a stream handler so that logging messages is
+    send to the terminal. The stream handler is attached to the root logger.
+    The logging messages from the `transformer` and `pytorch_lighting`
+    module set to propagate to the root logger.
+    If the handler had been created, this function returns the handler
+    created ealier instead.
 
     Returns:
         logging.StreamHandler: The created stream handler.
-    '''
+    """
     global stream_handler
 
     if stream_handler:
@@ -63,22 +62,29 @@ def add_stream_handler(level=logging.INFO):
 collect_handler = None
 
 def add_collect_handler(level=logging.NOTSET):
-    '''Create and return a ListHandler so that logging records with the attribute
-        ``collect=True'' is collected. The ListHandler is attached to the root logger.
-        If the handler had been created, this function returns the handler created
-        ealier instead.
+    """Create and return a ListHandler so that logging records with the attribute
+    `collect=True` is collected. The ListHandler is attached to the root logger.
+    If the handler had been created, this function returns the handler created
+    ealier instead.
+
+    To collect a log, set the key 'collect' with the value `True` in the `extra`
+    argument when logging. An example (similarly for logs of other levels):
+
+        logging.info('important message', extra={'collect': True})
 
     Returns:
         ListHandler: The created ListHandler.
-    '''
+    """
     global collect_handler
 
     if collect_handler:
         return collect_handler
     else:
+        logging.getLogger().setLevel(logging.NOTSET) # use handlers to control levels
+
         collect_handler = ListHandler(level=level)
         collect_handler.setFormatter(logging.Formatter(LOG_FORMAT))
         collect_handler.addFilter(lambda record: record.__dict__.get('collect', False))
         logging.getLogger().addHandler(collect_handler)
-    
+
     return collect_handler
