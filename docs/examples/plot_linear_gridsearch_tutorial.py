16e7f303ed6af01c54733cbc563681b7d4288f7d
Eleven Liu <jiejyunliu@gmail.com>
2023-02-08 07:30:58 +0800

    clean code

--- docs/examples/plot_linear_gridsearch_tutorial.py
+++ docs/examples/plot_linear_gridsearch_tutorial.py
@@ -10,7 +10,8 @@ Otherwise, you can read and preprocess the data as follow.
 """
 
 from sklearn.preprocessing import MultiLabelBinarizer
-from libmultilabel.linear.preprocessor import read_libmultilabel_format
+import libmultilabel.linear as linear
+from libmultilabel.linear.preprocessor import read_libmultilabel_format # code?
 
 train_data = read_libmultilabel_format('data/rcv1/train.txt')
 binarizer = MultiLabelBinarizer(sparse_output=True)
@@ -29,13 +30,12 @@ y = binarizer.transform(train_data['label']).astype('d')
 #
 # If you want to generate TF-IDF features with your own settings, consider
 
-import libmultilabel.linear as linear
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = TfidfVectorizer(max_features=20000, min_df=3)
 vectorizer.fit(train_data['text'])
 x = vectorizer.transform(train_data['text'])
-model = linear.train_1vsrest(y, x, '-s 2 -m 1')
+model = linear.train_1vsrest(y, x, '-s 2 -m 4')
 
 #######################################################################
 # We can then use the generated numerical features ``x`` as the input of
@@ -50,13 +50,11 @@ model = linear.train_1vsrest(y, x, '-s 2 -m 1')
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
-from libmultilabel.linear import MultiLabelEstimator
 
-estimator = MultiLabelEstimator(
-    options='-s 2 -m 1', linear_technique='1vsrest')
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer(max_features=20000, min_df=3)),
-    ('clf', estimator)
+    ('clf', linear.MultiLabelEstimator(
+        options='-s 2 -m 4', linear_technique='1vsrest'))
 ])
 
 ######################################################################
@@ -64,7 +62,8 @@ pipeline = Pipeline([
 # ``linear_technique`` is one of linear techniques: ``1vsrest``, ``thresholding``, ``cost_sensitive``,
 # ``cost_sensitive_micro``, and ``binary_and_mulitclass``.
 # In ``pipeline``, we specify settings used by the estimator.
-# For example, ``tfidf`` is the alias of ``TfidfVectorizer`` and ``clf`` is the alias of the estimator.
+# For example, ``tfidf`` indicates the vectorizer for generating TF-IDF features,
+# and ``clf`` is the estimator.
 #
 # We can then use the following code for training.
 pipeline.fit(train_data['text'], y)
@@ -73,16 +72,13 @@ pipeline.fit(train_data['text'], y)
 # Grid Search over Feature Generations and LIBLINEAR Options
 # -----------------------------------------------------------
 # To search for the best setting, we can employ ``GridSearchCV``.
-
-from libmultilabel.linear.sklearn_helper import GridSearchCV
-
 liblinear_options = ['-s 2 -c 0.5', '-s 2 -c 1', '-s 2 -c 2']
 parameters = {
     'clf__options': liblinear_options,
     'tfidf__max_features': [10000, 20000, 40000],
     'tfidf__min_df': [3, 5]
 }
-clf = GridSearchCV(pipeline, parameters, cv=5, n_jobs=4, verbose=1)
+clf = linear.GridSearchCV(pipeline, parameters, cv=5, n_jobs=4, verbose=1)
 clf = clf.fit(train_data['text'], y)
 
 ######################################################################
