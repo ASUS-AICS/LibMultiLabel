d2a1d0fdd4323b710ed77385f9479b251acbdec6
Eleven Liu <jiejyunliu@gmail.com>
2023-02-08 00:00:52 +0800

    Preprocess data

--- docs/examples/plot_linear_gridsearch_tutorial.py
+++ docs/examples/plot_linear_gridsearch_tutorial.py
@@ -6,6 +6,7 @@ This tutorial demonstrates feature generation and parameter selection for linear
 
 Here we show an example of training a linear text classifier with the rcv1 dataset.
 If you haven't downloaded it yet, see `Data Preparation  <../cli/linear.html#step-1-data-preparation>`_.
+Otherwise, you can read and preprocess the data as follow.
 """
 
 from sklearn.preprocessing import MultiLabelBinarizer
