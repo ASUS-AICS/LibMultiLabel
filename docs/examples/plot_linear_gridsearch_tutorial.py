8088dff45c975a4005c7a529133024eb854cf3d7
Eleven Liu <jiejyunliu@gmail.com>
2023-02-07 20:14:41 +0800

    Update comments, come back later.

--- docs/examples/plot_linear_gridsearch_tutorial.py
+++ docs/examples/plot_linear_gridsearch_tutorial.py
@@ -8,27 +8,33 @@ Our workflow is similiar to the one in sklearn.
 Here we show an example of training a linear text classifier with the rcv1 dataset.
 If you haven't downloaded it yet, see `Data Preparation  <../cli/linear.html#step-1-data-preparation>`_.
 
-Feature Generation and Setting Up a Linear Method
--------------------------------------------------
+Feature Generation
+------------------
 
 To use a linear method, we must convert ecah text to a vector of numerical features.
 If you decide to use the default setting (TF-IDF features) and specify the LIBLINEAR options
 used by linear methods by yourself, check `our quickstart <../api/api.html#quickstart>`_
 for easily conducting training and testing.
 
-To choose among different feature generations and LIBLINEAR options, we conduct a customized estimator
-for making LibMultiLabel methods in a sklearn Pipeline for a grid search.
+TBD. Sheng-Wei's version
 """
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+######################################################################
+# An Alternative Way for Using a Linear Method
+# --------------------------------------------
+# Besides the default way shown in `Feature Generation <#feature-generation>`_,
+# we can construct a sklearn estimator for training/prediction.
+# This way is used namely for parameter selection described later,
+# as the estimator makes LibMultiLabel methods in a sklearn Pipeline for a grid search.
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from libmultilabel.linear import MultiLabelEstimator
 
 estimator = MultiLabelEstimator(
     options='-s 2 -m 1', linear_technique='1vsrest')
 pipeline = Pipeline([
-    ('vect', CountVectorizer(max_features=20000, min_df=3)),
-    ('tfidf', TfidfTransformer()),
+    ('tfidf', TfidfVectorizer(max_features=20000, min_df=3)),
     ('clf', estimator)
 ])
 
@@ -36,13 +42,10 @@ pipeline = Pipeline([
 # For the estimator ``MultiLabelEstimator``, arguments ``options`` is a LIBLINEAR option, and
 # ``linear_technique`` is one of linear techniques: ``1vsrest``, ``thresholding``, ``cost_sensitive``,
 # ``cost_sensitive_micro``, and ``binary_and_mulitclass``.
-# In ``pipeline``, we specify ``tfidf`` as the way to convert texts to vectors.
-
-######################################################################
-# Grid Search over Feature Generations and LIBLINEAR Options
-# -----------------------------------------------------------
-# From the above construction of an estimator, because options are set, we can directly train a model with ``pipeline.fit``.
-# We format labels into a 0/1 sparse matrix with ``MultiLabelBinarizer``.
+# In ``pipeline``, we specify settings used by the estimator.
+# For example, ``tfidf`` is the alias of ``TfidfVectorizer`` and ``clf`` is the alias of the estimator.
+#
+# We can then use the following code for training.
 
 from sklearn.preprocessing import MultiLabelBinarizer
 from libmultilabel.linear.preprocessor import read_libmultilabel_format
@@ -57,6 +60,14 @@ train_labels = binarizer.transform(train_data['label']).astype('d')
 pipeline.fit(train_data['text'], train_labels)
 
 ######################################################################
+# Grid Search over Feature Generations and LIBLINEAR Options
+# -----------------------------------------------------------
+# From the above construction of an estimator, because options are set, we can directly train a model with ``pipeline.fit``.
+# We format labels into a 0/1 sparse matrix with ``MultiLabelBinarizer``.
+
+
+
+######################################################################
 # However, to search for the best setting, we can employ ``GridSearchCV``.
 
 from libmultilabel.linear.sklearn_helper import GridSearchCV
@@ -64,8 +75,8 @@ from libmultilabel.linear.sklearn_helper import GridSearchCV
 liblinear_options = ['-s 2 -c 0.5', '-s 2 -c 1', '-s 2 -c 2']
 parameters = {
     'clf__options': liblinear_options,
-    'vect__max_features': [10000, 20000, 40000],
-    'vect__min_df': [3, 5]
+    'tfidf__max_features': [10000, 20000, 40000],
+    'tfidf__min_df': [3, 5]
 }
 clf = GridSearchCV(pipeline, parameters, cv=5, n_jobs=4, verbose=1)
 clf = clf.fit(train_data['text'], train_labels)
