390384c0968b14dd5f44c1cbe72f5310e575ce0c
Gordon119 <b09902131@ntu.edu.tw>
2023-02-15 05:56:23 +0000

    update NN part

--- docs/examples/plot_dataset_tutorial.py
+++ docs/examples/plot_dataset_tutorial.py
@@ -4,7 +4,7 @@ An Example of Using Data Stored in Different Forms.
 
 Different data sets are stored in various structures and formats.
 To apply LibMultiLabel with any of them, one must convert the data to a form accepted by the library first.  
-In this tutorial, we demonstrate an example of conversion of a hugging face data set.
+In this tutorial, we demonstrate an example of converting a hugging face data set.
 Before we start, note that LibMultiLabel format consists of IDs (optional), labels, and raw texts. 
 `Here <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/cli/ov_data_format.html#libmultilabel-format>`_ are more details. 
 
@@ -32,8 +32,8 @@ data_sets['test'] = load_dataset('tweet_eval', 'emoji', split='test')
 ######################################################################
 # Convert to LibMultiLabel format
 # --------------------------------------------------
-# We begin with loading Hugging Face's data set to Pandas' structure by the function ``DataFrame``. 
-# In consistence with our `linear model quickstart <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/auto_examples/plot_linear_quickstart.html>`_ which don't have validation set,
+# We load Hugging Face's data set to Pandas' structure by the function ``DataFrame``. 
+# In consistent with our `linear model quickstart <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/auto_examples/plot_linear_quickstart.html>`_, which does not need a validation set,
 # we use ``pandas.concat`` to merge training and validation sets and use ``reset_index`` to add the new indices to rows.
 
 for split in ['train', 'val', 'test']:
@@ -50,19 +50,19 @@ data_sets['test'] = data_sets['test'].reset_index()
 #   index  label                                               text
 #       0     12  Sunday afternoon walking through Venice in the...  
 #
-# Next, we train and make prediction with the data set. The detailed explanation is in our `linear model quickstart <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/auto_examples/plot_linear_quickstart.html>`_. 
+# Next, we train and make prediction with the data set using linear model. The detailed explanation is in our `linear model quickstart <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/auto_examples/plot_linear_quickstart.html>`_. 
 # The difference between here and the quickstart is that the ``data_format`` option should be ``dataframe`` because the data set is a dataframe now.
 
 import libmultilabel.linear as linear
 
 preprocessor = linear.Preprocessor(data_format='dataframe')
-datasets = preprocessor.load_data(data_sets['train'],
-                                  data_sets['test'])
-                     
-model = linear.train_1vsrest(datasets['train']['y'], datasets['train']['x'], '')
-preds = linear.predict_values(model, datasets['test']['x'])
-metrics = linear.get_metrics(metric_threshold=0,
-                             monitor_metrics=['Macro-F1', 'Micro-F1', 'P@1'],
-                             num_classes=datasets['test']['y'].shape[1])
-target = datasets['test']['y'].toarray()
-metrics.update(preds, target)
+datasets = preprocessor.load_data(data_sets['train'], data_sets['test'])
+
+###############################################################################
+# Also, if you want to use a NN model,
+# use ``load_datasets`` from ``datasets`` and change the data to the dataframes we created.
+# Here is the modification of our `Bert model quickstart <https://www.csie.ntu.edu.tw/~cjlin/libmultilabel/auto_examples/plot_BERT_quickstart.html>`_. 
+
+from datasets import load_dataset
+
+datasets = load_datasets(data_sets['train'], data_sets['test'], tokenize_text=False)
