919d06d3b4ce516e187548d4d1bf787bc62785b9
Gordon119 <b09902131@ntu.edu.tw>
2023-02-23 10:17:24 +0000

    add example

--- docs/examples/plot_linear_quickstart.py
+++ docs/examples/plot_linear_quickstart.py
@@ -66,8 +66,9 @@ prediction = preprocessor.binarizer.inverse_transform(preds)
 ######################################################################
 # The result of first row looks like:
 #
-#   >>>print(prediction[0])
-#   ...('GCAT', 'GSPO')
+#   >>> print(prediction[0])
+#   ... ('GCAT', 'GSPO')
+#
 # To see how well we performed, we may want to check various
 # metrics with the test set.
 # For that we may use:
