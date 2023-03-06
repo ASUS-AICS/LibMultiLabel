fa2f5bb48f62dcfa125c85b65f1465ecb567f4ef
Sinacam <stinkingmadgod@gmail.com>
2023-03-06 18:29:13 +0800

    fixed power

--- libmultilabel/linear/kmeans.py
+++ libmultilabel/linear/kmeans.py
@@ -20,7 +20,7 @@ def spherical(x: sparse.csr_matrix, k: int, max_iter: int, tol: float) -> np.nda
     centroids = x[init]
     prev_sim = np.inf
     for _ in range(max_iter):
-        similarity = x * centroids.transpose()
+        similarity = x * centroids.T
         cluster = similarity.argmax(axis=1).A1
 
         avg_sim = np.take_along_axis(
@@ -44,7 +44,7 @@ def lloyd(x: sparse.csr_matrix, k: int, max_iter: int, tol: float) -> np.ndarray
     """Perform lloyd's algorithm k-means clustering on x.
 
     Args:
-        x (sparse.csr_matrix): Matrix with dimensions number of points * dimension of underlying space.
+        x (sparse.csr_matrix): Matrix with dimensions number of points * dimension of underlying space. Each row must be L2 normalized.
         k (int): Number of clusters.
         max_iter (int): Maximum number of iterations.
         tol (float): Stopping tolerance. Lower for more optimal clustering but longer run time.
@@ -56,8 +56,8 @@ def lloyd(x: sparse.csr_matrix, k: int, max_iter: int, tol: float) -> np.ndarray
     centroids = x[init]
     prev_dist = np.inf
     for _ in range(max_iter):
-        similarity = x * centroids.transpose()
-        norm_square = (centroids ** 2).sum(axis=1).T
+        similarity = x * centroids.T
+        norm_square = centroids.power(2).sum(axis=1).T
         distance_square = norm_square - 2*similarity
         cluster = distance_square.argmin(axis=1).A1
 
