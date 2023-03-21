6c9bdd85f38ae8e9df769e36c7e5f102ea8048a4
Sheng-Wei Chen <will945945945@gmail.com>
2023-03-21 15:44:53 +0800

    update draft

--- docs/examples/plot_linear_tree_tutorial.py
+++ docs/examples/plot_linear_tree_tutorial.py
@@ -1,29 +1,14 @@
 """
-Tree-Based Linear Model for Extreme Multi-Label Classification.
-===============================================================
+Accelerated Training for Multi-Label Classification
+===================================================
 
-In this tutorial, we show how to use our tree-based linear solver for a multi-label classification problem, 
-and lightly explain why a tree-based setting can reduce the training time, especially the number of labels :math:`m` is extremely large. 
+Multi-label classification is an important task in machine learning that involves predicting multiple labels for a given input. 
+While linear models are commonly used for this task, training on large-scale datasets with many labels can be time-consuming. 
+In this tutorial, we will introduce an accelerated training algorithm for multi-label classification that can reduce training time on such datasets.
 
-Traditionally, we can treat each label as an independent binary classification problem in multi-label classification problem, 
-i.e., we have to solve :math:`m` binary classification problems, and this naive method is called `one-versus-rest`.
-`Our quickstart of linear model <plot_linear_quickstart.html>`_ is an example for using one-versus-rest linear solver,
-and the training complexity on a data set 
-
-.. math ::
-
-  (X, Y) \in (\mathbb{R}^{l \\times n}, \mathbb{R}^{l \\times m})
-
-is roughly :math:`O( m \\times \\textrm{nnz of } X  )`. 
-Therefore, when the number of labels :math:`m` is a extremely large number,
-the training time grows up to a painful long time.  
-Let us show this issue by training a data set `EUR-Lex` with 
-
-.. math::
-
-  l = 15449, n=186104, m=3956 \\textrm{ and nnz of } X = 4160670,
-
-where the code is similar to `our quickstart of linear model <plot_linear_quickstart.html>`_ instead of the data set.
+To demonstrate the effectiveness of our approach, we will use the EUR-Lex dataset with 3,956 labels, 
+which can be downloaded from `LIBSVM Data Sets <https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multilabel.html>`_. 
+We will provide code that is similar to `our quickstart of linear model <plot_linear_quickstart.html>`_, but with the added benefits of faster training time.
 """
 
 import libmultilabel.linear as linear
@@ -34,61 +19,42 @@ datasets = preprocessor.load_data('data/eur-lex/train.txt',
                                   'data/eur-lex/test.txt')
 
 training_start = time.time()
+# Original API from quickstart of linear model
 model_OVR = linear.train_1vsrest(datasets['train']['y'],
                                  datasets['train']['x'],
                                  '-s 2')
 training_end = time.time()
 print("Training time of one-versus-rest: {:10.2f}".format(training_end-training_start))
 
-######################################################################
-# In our machine with CPU AMD-7950X, a one-versus-rest method requires :math:`578.30` seconds when the solver applies multi-core version LIBLINEAR.
-# 
-# To conquer the explosion of training time, we can divid the labels to :math:`k` clusters, 
-# where :math:`k` is much smaller than :math:`m` (e.g., taking :math:`k=100`), and train a subproblem with :math:`k` metalabels via one-versus-rest method. 
-# That is, the data set on this subproblem becomes 
-# 
-# .. math::
-#
-#  (X, \tilde{Y}) \in (\mathbb{R}^{l \times n}, \mathbb{R}^{l \times k})
-#  
-# and an given instance :math:`\boldsymbol{x}` has :math:`j\text{th}` metalabel means that 
-# this data :math:`\boldsymbol{x}` has some labels in the :math:`j\text{th}` cluster.
-# If the number of labels in a cluster is still large, it can then be divided again.    
-# Therefore, this divid-and-conquer step can construct a tree for label, and we call `tree-based linear method` here. 
-# 
-# Actually, we have :math:`k` binary classification problems in each node of tree, so the total number of binary problems is approximate to :math:`2m`.
-# However, except for the root node, the instances are much less than :math:`l` (idealy :math:`l/k` ), 
-# which implies the total training complexity becomes 
-# 
-# .. math::
-# 
-#  O( \sum_{s=0}^{d} k^{s} \times k \times  \frac{ \text{nnz of } X }{ k^{s} } ) 
-#    \approx O( k \times (\log_{k} (m) + 1 ) \times \text{nnz of } X )
-#
-# if the label tree is a balanced :math:`d-\text{level}` tree and the nnz of :math:`X` is uniformly distributed.
-#
-# In our implementation, we mainly consider the method `Bonsai` :cite:t:`SK20a`, and you can use the solver by the following code.
-
-
 training_start = time.time()
+# New API for accelerated training a linear model
 model_tree = linear.train_tree(datasets['train']['y'],
                                datasets['train']['x'],
                                '-s 2')
 training_end = time.time()
 print("Training time of tree-based: {:10.2f}".format(training_end-training_start))
 
-
 ######################################################################
-# The training time is reduced to :math:`144.37` seconds. 
+# On our machine with an AMD-7950X CPU, we observed that the `train_1vsrest` function required `578.30` seconds to complete the training on the EUR-Lex dataset, 
+# while the `train_tree` function only required `144.37` seconds. 
+# This demonstrates the significant reduction in training time that can be achieved by using the `train_tree` algorithm.
+# 
+# .. note::
 #
-# For the performance, a tree-based method usually worse than one-versus-rest method. 
-# In this example, the evaluation can be calculated by the following code.
+#   The `train_tree` function in this tutorial is based on the work of :cite:t:`SK20a`, 
+#   who introduced some label approximation techniques to improve the training speed of their model, known as Bonsai. 
+#   While the details of these techniques are beyond the scope of this tutorial, 
+#   we have incorporated them into our implementation to provide a faster training algorithm for multi-label classification problems.
+#
+# While approximation methods may lead to slightly worse performance compared to the original algorithm, 
+# we found that the difference in performance between train_tree and train_1vsrest was minimal in our experiments. 
+# To evaluate the performance of our models on the EUR-Lex dataset, we provide the following code for calculating evaluation metrics.
 
 preds_OVR = linear.predict_values(model_OVR, datasets['test']['x'])
 preds_tree = linear.predict_values(model_tree, datasets['test']['x'])
 
 metrics = linear.get_metrics(metric_threshold=0,
-                             monitor_metrics=['Macro-F1', 'Micro-F1', 'P@1', 'P@3', 'P@5'],
+                             monitor_metrics=['P@1', 'P@3', 'P@5'],
                              num_classes=datasets['test']['y'].shape[1])
 
 target = datasets['test']['y'].toarray()
@@ -99,10 +65,15 @@ metrics.update(preds_tree, target)
 print("Evaluation of tree:", metrics.compute())
 
 ######################################################################
-# The results will look similar to::
+# Using the evaluation code provided, we can calculate the following metrics for our trained models:
 #
-#     Evaluation of OVR: {'Macro-F1': 0.16310265482983555, 'Micro-F1': 0.5125155218613162, 'P@1': 0.833117723156533, 'P@3': 0.6989219491159984, 'P@5': 0.585666235446313}
-#     Evaluation of tree: {'Macro-F1': 0.08138827935674002, 'Micro-F1': 0.05018160714821745, 'P@1': 0.825614489003881, 'P@3': 0.6947822337214317, 'P@5': 0.5803622250970245}
+#.. code-block::
 #
+#   Evaluation of OVR: {'P@1': 0.833117723156533, 'P@3': 0.6989219491159984, 'P@5': 0.585666235446313}
+#   Evaluation of tree: {'P@1': 0.825614489003881, 'P@3': 0.6947822337214317, 'P@5': 0.5803622250970245}
+#
+#As we can see, while there is a slight difference in performance between the two methods, it is not significant. 
+#Therefore, the train_tree algorithm can be considered a viable option 
+#for training linear models on large-scale multi-label classification problems with a reduced training time.
 #
 #.. bibliography::
