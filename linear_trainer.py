349ecdeb5b7afe4a13d273dd993adfbff424cb97
Gordon119 <gordonsmart119@gmail.com>
2023-04-20 18:34:15 +0800

    update api usage

--- linear_trainer.py
+++ linear_trainer.py
@@ -8,7 +8,6 @@ import libmultilabel.linear as linear
 from libmultilabel.common_utils import argsort_top_k, dump_log
 from libmultilabel.linear.utils import LINEAR_TECHNIQUES
 
-
 def linear_test(config, model, datasets):
     metrics = linear.get_metrics(
         config.monitor_metrics,
@@ -16,8 +15,9 @@ def linear_test(config, model, datasets):
         multiclass=model.name == 'binary_and_multiclass'
     )
     num_instance = datasets['test']['x'].shape[0]
-    assert not (config.save_positive_predictions and config.save_k_predictions >
-                0), "If save_k_predictions is larger than 0, only top k labels are saved. Save all labels with decision value larger than 0 by using save_all and save_k_predictions=0."
+    assert not (config.save_positive_predictions and config.save_k_predictions > 0), """
+        If save_k_predictions is larger than 0, only top k labels are saved.
+        Save all labels with decision value larger than 0 by using save_all and save_k_predictions=0."""
     k = config.save_k_predictions
     if k > 0:
         idx = np.zeros((num_instance, k), dtype='i')
@@ -25,22 +25,19 @@ def linear_test(config, model, datasets):
     else:
         idx = []
         scores = []
-
     for i in tqdm(range(ceil(num_instance / config.eval_batch_size))):
         slice = np.s_[i*config.eval_batch_size:(i+1)*config.eval_batch_size]
         preds = linear.predict_values(model, datasets['test']['x'][slice])
         target = datasets['test']['y'][slice].toarray()
         metrics.update(preds, target)
-
         if k > 0:
-            idx[slice] = argsort_top_k(preds, k, axis=1)
-            scores[slice] = np.take_along_axis(preds, idx[slice], axis=1)
-
+            res = linear.get_labels(preds, config.save_positive_predictions, config.save_k_predictions)
+            idx[slice] = res[0]
+            scores[slice] = res[1]
         elif config.save_positive_predictions:
-            for i_pred in preds:
-                pos_idx = i_pred > 0
-                idx.append(pos_idx)
-                scores.append(i_pred[pos_idx])
+            res = linear.get_labels(preds, config.save_positive_predictions, config.save_k_predictions)
+            idx.extend(res[0])
+            scores.extend(res[1])
     metric_dict = metrics.compute()
     return metric_dict, idx, scores
 
