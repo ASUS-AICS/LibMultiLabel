0775b78a6d8f2393537eab3295b0bc07d69dc461
Gordon119 <gordonsmart119@gmail.com>
2023-04-21 02:08:39 +0800

    seperate dump

--- linear_trainer.py
+++ linear_trainer.py
@@ -5,8 +5,9 @@ import numpy as np
 from tqdm import tqdm
 
 import libmultilabel.linear as linear
-from libmultilabel.common_utils import argsort_top_k, dump_log
+from libmultilabel.common_utils import dump_log
 from libmultilabel.linear.utils import LINEAR_TECHNIQUES
+
 
 def linear_test(config, model, datasets):
     metrics = linear.get_metrics(
@@ -17,7 +18,7 @@ def linear_test(config, model, datasets):
     num_instance = datasets['test']['x'].shape[0]
     assert not (config.save_positive_predictions and config.save_k_predictions > 0), """
         If save_k_predictions is larger than 0, only top k labels are saved.
-        Save all labels with decision value larger than 0 by using save_all and save_k_predictions=0."""
+        Save all labels with decision value larger than 0 by using save_positive_predictions and save_k_predictions=0."""
     k = config.save_k_predictions
     if k > 0:
         idx = np.zeros((num_instance, k), dtype='i')
@@ -31,13 +32,14 @@ def linear_test(config, model, datasets):
         target = datasets['test']['y'][slice].toarray()
         metrics.update(preds, target)
         if k > 0:
-            res = linear.get_labels(preds, config.save_positive_predictions, config.save_k_predictions)
+            res = linear.get_topk_labels(
+                preds, config.save_k_predictions)
             idx[slice] = res[0]
             scores[slice] = res[1]
         elif config.save_positive_predictions:
-            res = linear.get_labels(preds, config.save_positive_predictions, config.save_k_predictions)
-            idx.extend(res[0])
-            scores.extend(res[1])
+            res = linear.get_positive_labels(preds)
+            idx.append(res[0])
+            scores.append(res[1])
     metric_dict = metrics.compute()
     return metric_dict, idx, scores
 
@@ -87,10 +89,18 @@ def linear_run(config):
         dump_log(config=config, metrics=metric_dict,
                  split='test', log_path=config.log_path)
         print(linear.tabulate_metrics(metric_dict, 'test'))
-        if config.save_positive_predictions or config.save_k_predictions > 0:
+        if config.save_k_predictions > 0:
             with open(config.predict_out_path, 'w') as fp:
                 for idx, score in zip(idx, scores):
                     out_str = ' '.join([f'{i}:{s:.4}' for i, s in zip(
                         preprocessor.label_mapping[idx], score)])
                     fp.write(out_str+'\n')
+            logging.info(f'Saved predictions to: {config.predict_out_path}')
+        if config.save_positive_predictions:
+            with open(config.predict_out_path, 'w') as fp:
+                for b_idx, b_score in zip(idx, scores):
+                    for idx, score in zip(b_idx, b_score):
+                        out_str = ' '.join([f'{i}:{s:.4}' for i, s in zip(
+                            preprocessor.label_mapping[idx], score)])
+                        fp.write(out_str+'\n')
             logging.info(f'Saved predictions to: {config.predict_out_path}')
