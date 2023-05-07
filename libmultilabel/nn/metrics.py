cd94d6889e1b16ae8017e38075bb143474ff714a
Eleven Liu <jiejyunliu@gmail.com>
2023-05-07 23:39:38 +0800

    matched_top_k -> k

--- libmultilabel/nn/metrics.py
+++ libmultilabel/nn/metrics.py
@@ -214,20 +214,20 @@ def get_metrics(metric_threshold, monitor_metrics, num_classes, top_k=None):
 
         if match_top_k:
             metric_abbr = match_top_k.group(1)  # P, R, PR, or nDCG
-            matched_top_k = int(match_top_k.group(2))
-            if matched_top_k > num_classes or (top_k is not None and matched_top_k > top_k):
+            k = int(match_top_k.group(2))
+            if k > num_classes or (top_k is not None and k > top_k):
                 raise ValueError(
-                    f'Invalid metric: {metric}. top_k ({matched_top_k}) is greater than {top_k} or num_classes({num_classes}).')
+                    f'Invalid metric: {metric}. top_k ({k}) is greater than {top_k} or num_classes({num_classes}).')
             if metric_abbr == 'P':
                 metrics[metric] = Precision(
-                    num_classes, average='samples', top_k=matched_top_k)
+                    num_classes, average='samples', top_k=k)
             elif metric_abbr == 'R':
                 metrics[metric] = Recall(
-                    num_classes, average='samples', top_k=matched_top_k)
+                    num_classes, average='samples', top_k=k)
             elif metric_abbr == 'RP':
-                metrics[metric] = RPrecision(top_k=matched_top_k)
+                metrics[metric] = RPrecision(top_k=k)
             elif metric_abbr == 'nDCG':
-                metrics[metric] = NDCG(top_k=matched_top_k)
+                metrics[metric] = NDCG(top_k=k)
                 # The implementation in torchmetrics stores the prediction/target of all batches,
                 # which can lead to CUDA out of memory.
                 # metrics[metric] = RetrievalNormalizedDCG(k=top_k)
