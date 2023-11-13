3140278b351fd54b376db3e74f5d672bb5f6177c
Eleven Liu <jiejyunliu@gmail.com>
2023-11-13 14:04:26 +0800

    Update copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "LibMultiLabel"
-copyright = "2021, LibMultiLabel Project"
+copyright = "2023, LibMultiLabel Project"
 author = "LibMultiLabel Authors"
 
 
