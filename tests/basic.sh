f7ffe1d2ac837ac138129d840df008d6edb5134f
Dongli He <dongli.he@mbzuai.ac.ae>
2024-03-26 15:24:23 +0400

    Add new test parameters val_size and save_k_predictions

--- tests/basic.sh
+++ tests/basic.sh
@@ -119,6 +119,7 @@ find example_config -name "*.yml" ${filters[@]} -type f -print0 |
         echo "Running $config"
         stderr=$(python $script --config "$config" --epochs 1 \
             --result_dir "$result_dir" --embed_cache_dir data \
+            --val_size 0.2 --save_k_predictions 2 \
             --cpu 2>&1 > /dev/null)
         if [[ $? -ne 0 ]]; then
             echo "$stderr" >&2
